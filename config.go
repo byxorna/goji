c9e43eef70ab06db3843552351483e190b200b18
Gabe Conradi <gabe@tumblr.com>
2015-01-30 08:33:23 -0500

    add sample template

--- config.go
+++ config.go
@@ -7,8 +7,9 @@ import (
 )
 
 type Service struct {
-	Vhost string `json:"vhost"`
-	AppId string `json:"app-id"`
+	Vhost string  `json:"vhost"`
+	AppId string  `json:"app-id"`
+	Tasks *[]Task `json:"-"`
 	//TODO add config for healthchecking, type of connection (HTTP/TCP), etc
 	//TODO possibly add configurable domains
 }
--- template.go
+++ template.go
@@ -5,12 +5,17 @@ import (
 	"text/template"
 )
 
+type TemplateData struct {
+	Services []Service
+}
+
 // given tasks and a template, do the templating
 func Template(tasks []Task, templateFile string) error {
 	tmpl, err := template.ParseFiles(templateFile)
 	if err != nil {
 		return err
 	}
+	//TODO write this to the filesystem
 	err = tmpl.Execute(os.Stdout, tasks)
 	if err != nil {
 		return err
